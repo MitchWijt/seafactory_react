7221fe14c4fa0e6c340b4999fedcec526ac10d79
umairx97 <umairahmedbajwa97@gmail.com>
2021-05-21 06:28:19 +0500

    refactor: move login code

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,32 +1,5 @@
-import axios from 'axios'
-import { getToken, setToken } from './api'
+import { getToken } from './api'
 
-export const handleLogin = async (data, loginType) => {
-  try {
-    const { email, password } = data
-    const url = getReqUrlBasedOnLoginType(loginType)
-
-    const res = await axios.get(url, {
-      headers: {
-        email: email,
-        password: password,
-        'Content-Type': 'application/json'
-      }
-    })
-    const jwt = loginType === 'admin' ? res.data.jwt : res.data.jwtStaff
-    await setToken(jwt)
-  } catch (e) {
-    console.log(e.response.data.description)
-  }
-}
-
-const getReqUrlBasedOnLoginType = (loginType) => {
-  if (loginType === 'admin') {
-    return '/login/admin'
-  } else {
-    return '/login/staff'
-  }
-}
 export const handleLogout = async () => {
   // sets bearer token which removes the JWT token from the cookie.
   await getToken()
