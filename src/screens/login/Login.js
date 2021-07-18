c1cf7c3a2f759b79832487da6e2c4af76aa0ca08
umairx97 <umairahmedbajwa97@gmail.com>
2021-07-18 05:54:43 +0500

    refactor: login.js

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -7,30 +7,7 @@ import { login } from '../../services/api'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
 
-const Login = (props) => {
-  return (
-    <>
-      <Header />
-      <div className='container'>
-        <div className='register-container'>
-          <div className='register-text'>
-            <h1 className='heavy-title'>Login</h1>
-          </div>
-          <div className='register-box-container'>
-            <RegisterBackgroundImage url='/assets/login_img.jpg' />
-            <div className='register-text-container'>
-              <div className='register-list-text'>
-                <LoginForm />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  )
-}
-
-const LoginForm = () => {
+const Login = () => {
   const validationSchema = Yup.object({
     email: Yup.string()
       .email('Invalid email address')
@@ -53,30 +30,54 @@ const LoginForm = () => {
 
   return (
     <>
-      <Formik
-        validateOnBlur={false}
-        validateOnChange={false}
-        initialValues={{ email: '', password: '' }}
-        validationSchema={validationSchema}
-        onSubmit={onSubmit}
-      >
-        {({
-          values,
-          errors,
-          handleChange,
-          handleSubmit
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <FormInput error={errors.email} type='email' placeholder='Email address' name='email' onChange={handleChange} value={values.email} />
-            <FormInput error={errors.password} type='password' placeholder='Password' name='password' onChange={handleChange} value={values.password} />
-            <div className='register-cta-button'>
-              <Button type='submit' category='cta' fontType='bold' text='Continue' />
+      <Header />
+      <div className='container'>
+        <div className='register-container'>
+          <div className='register-text'>
+            <h1 className='heavy-title'>Login</h1>
+          </div>
+          <div className='register-box-container'>
+            <RegisterBackgroundImage url='/assets/login_img.jpg' />
+            <div className='register-text-container'>
+              <div className='register-list-text'>
+                <>
+                  <Formik
+                    validateOnBlur={false}
+                    validateOnChange={false}
+                    initialValues={{ email: '', password: '' }}
+                    validationSchema={validationSchema}
+                    onSubmit={onSubmit}
+                  >
+                    {({
+                      values,
+                      errors,
+                      handleChange,
+                      handleSubmit
+                    }) => (
+                      <form onSubmit={handleSubmit}>
+                        <FormInput error={errors.email} type='email' placeholder='Email address' name='email' onChange={handleChange} value={values.email} />
+                        <FormInput error={errors.password} type='password' placeholder='Password' name='password' onChange={handleChange} value={values.password} />
+                        <div className='register-cta-button'>
+                          <Button type='submit' category='cta' fontType='bold' text='Continue' />
+                        </div>
+                      </form>
+                    )}
+                  </Formik>
+                </>
+              </div>
             </div>
-          </form>
-        )}
-      </Formik>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
+
+// const LoginForm = () => {
+
+//   return (
+
+//   )
+// }
 
 export default Login
